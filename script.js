1875744333fdd761b4f61f43ee93ab2b34068cc8
Vien Truong <vientruong030900@gmail.com>
2022-04-05 00:09:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuBar = document.querySelector(".menu-bar")
+menuBar.addEventListener("click", function() {
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
+})
+const toP = document.querySelector(".top")
+window.addEventListener("scroll", function() {
+    const x = this.pageYOffset
+    if (x > 80) { toP.classList.add("active") } else { toP.classList.remove("active") }
+})
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click", function(x) {
+    if (x.target.classList.contains("menu-button")) {
+        const Target = x.target.getAttribute("data-title");
+        //console.log(Target)
+        menuTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+
+        const menuItem = document.querySelector(".menu");
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active")
+    }
+})
